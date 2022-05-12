431b235e9ab5288d5112387512d4bc157b8a5890
Harun-or-rashid <harun35-2093@diu.edu.bd>
2022-05-12 17:09:25 +0600

    state used

--- src/components/ExpenseItem.js
+++ src/components/ExpenseItem.js
@@ -1,17 +1,24 @@
-import { ReactDOM } from "react";
+import { ReactDOM, useState } from "react";
 import Card from "./Card";
 import ExpenseDate from "./ExpenseDate";
 import Expense from "./Expense";
 import './ExpenseItem.css'
 import './Card.css'
 const ExpenseItem=(props)=>{
-   
+   const onchangeHandler=()=>{
+       console.log("hello")
+   }
+   const [title,SetTitle]=useState(props.title)
+   const titleHandler=()=>{
+    SetTitle("ji");  
+   }
 return(
     <Card className="expense-item">
         <ExpenseDate date={props.date}></ExpenseDate>
         <div className="expense-item__description">
-            <h2>{props.title}</h2>
+            <h2 onClick={titleHandler}>{title}</h2>
             <div className="expense-item__price">{props.amount}</div>
+            <button className="expense-item__button" onClick={onchangeHandler}>Change</button>
         </div>
     </Card>
 )
