fa802ceb6f38d3c27f9766051da574f727967ad5
Harun-or-rashid <harun35-2093@diu.edu.bd>
2022-05-14 04:07:10 +0600

    form type handler and state added

--- src/components/ExpenseForm.js
+++ src/components/ExpenseForm.js
@@ -1,12 +1,25 @@
-import React from "react";
+import React, { useState } from "react";
 import './ExpenseForm.css'
 const ExpenseForm=()=>{
+const [title,setTitle]=useState('');
+const [amount,setAmount]=useState('');
+const [date,setDate]=useState('');
 
+const titleChangedHandler=(event)=>{
+    setTitle(event.target.value)
+    console.log(event)
+}
+const amountChangedHandler=(event)=>{
+setAmount(event.target.value)
+}
+const dateChangedHandler=(event)=>{
+    setDate(event.target.value)
+}
     return <form>
             <div className="new-expense__controls">
                 <div className="new-expense__control">
                     <label>Title</label>
-                    <input type="text" ></input>
+                    <input onChange={titleChangedHandler} type="text" ></input>
                 </div>
                 <div className="new-expense__control">
                     <label>Amount</label>
