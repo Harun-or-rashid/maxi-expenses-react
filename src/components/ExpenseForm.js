83851b2151814b414aeda9933ca3b6d2c7902835
Harun-or-rashid <harun35-2093@diu.edu.bd>
2022-05-14 04:10:14 +0600

    form type event  added

--- src/components/ExpenseForm.js
+++ src/components/ExpenseForm.js
@@ -23,11 +23,11 @@ const dateChangedHandler=(event)=>{
                 </div>
                 <div className="new-expense__control">
                     <label>Amount</label>
-                    <input type="number" min="0.01" step="0.01" ></input>
+                    <input onChange={amountChangedHandler} type="number" min="0.01" step="0.01" ></input>
                 </div>
                 <div className="new-expense__control">
                     <label>Date</label>
-                    <input type="date" min="2019-01-01" max="2022-12-31" ></input>
+                    <input onChange={dateChangedHandler} type="date" min="2019-01-01" max="2022-12-31" ></input>
                 </div>
             </div>
             <div className="new-expense__actions">
